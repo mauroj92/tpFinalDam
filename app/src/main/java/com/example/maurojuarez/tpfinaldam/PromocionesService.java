f990925d5cb4d378710321ee34a79fc55c637927
MauroJ <mauroojuarez@gmail.com>
2018-02-13 19:28:47 -0300

    Notificaciones push

--- app/src/main/java/com/example/maurojuarez/tpfinaldam/PromocionesService.java
+++ app/src/main/java/com/example/maurojuarez/tpfinaldam/PromocionesService.java
@@ -3,8 +3,9 @@ package com.example.maurojuarez.tpfinaldam;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -12,27 +13,20 @@ import com.google.firebase.messaging.RemoteMessage;
 public class PromocionesService extends FirebaseMessagingService {
     private static final String TAG = "FCM Service";
 
-    public PromocionesService() {
-    }
-
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        super.onMessageReceived(remoteMessage);
-        int mNotificationId = 001;
-        NotificationManager mNotifyMgr =
-                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
-        String body = remoteMessage.getNotification().getBody();
 
+        Intent intent = new Intent(this, ListaPlatos.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder noBuilder = new NotificationCompat.Builder(this);
+        noBuilder.setContentTitle("PROMOCIÓN");
+        noBuilder.setContentText(remoteMessage.getNotification().getBody());
+        noBuilder.setAutoCancel(true);
+        noBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        noBuilder.setContentIntent(pendingIntent);
 
-
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(android.R.drawable.stat_notify_chat)
-                        .setContentTitle("NUEVA PROMOCIÓN")
-                        .setContentText("Apurate");
-
-        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,noBuilder.build());
     }
 }
