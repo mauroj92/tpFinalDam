91b415aef3858b09d2a10b575290b69392c25635
MauroJ <mauroojuarez@gmail.com>
2018-02-13 16:16:30 -0300

    Mensajes push

---
+++ app/src/main/java/com/example/maurojuarez/tpfinaldam/PromocionesInstanceId.java
@@ -0,0 +1,8 @@
+package com.example.maurojuarez.tpfinaldam;
+
+/**
+ * Created by Mauro Juarez on 13 feb 2018.
+ */
+
+public class PromocionesInstanceId {
+}
---
+++ app/src/main/java/com/example/maurojuarez/tpfinaldam/PromocionesService.java
@@ -0,0 +1,13 @@
+package com.example.maurojuarez.tpfinaldam;
+
+
+import android.content.Intent;
+import android.os.IBinder;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class PromocionesService extends FirebaseMessagingService {
+    public PromocionesService() {
+    }
+
+
+}
